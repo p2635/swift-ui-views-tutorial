0f9bbc503af2fca71d6ada5c302f708cdd36d9fd
p2635 <4522927+p2635@users.noreply.github.com>
2023-01-30 21:32:42 +0000

    Update README

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Phil on 30/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
